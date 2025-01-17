ee1a79c546f930b80d6bf1241d9778acc509eb98
Shashank6296 <109429034+Shashank6296@users.noreply.github.com>
2025-01-17 15:50:56 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const eventRoutes = require('./routes/eventRoutes');
 dotenv.config();
 const app = express();
 
-app.use(cors({ origin: "https://api-t99t.vercel.app/" }));
+app.use(cors({ origin: "https://api-1-lemt.onrender.com" }));
 app.use(express.json());
 
 mongoose
